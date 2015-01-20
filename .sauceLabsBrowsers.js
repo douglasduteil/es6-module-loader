6deed401fe8f5ea237424e894bc5e86d4c234090
Douglas Duteil <douglasduteil@gmail.com>
2015-01-21 00:08:18 +0100

    SAVE

--- .sauceLabsBrowsers.js
+++ .sauceLabsBrowsers.js
@@ -1,38 +1,133 @@
 //
 
-module.exports = {
-  'SL_Chrome': {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '34'
-  },
-  'SL_Firefox': {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '26'
-  },
-  'SL_Safari': {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.9',
-    version: '7'
-  },
-  'SL_IE_9': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 2008',
-    version: '9'
-  },
-  'SL_IE_10': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 2012',
-    version: '10'
-  },
-  'SL_IE_11': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
+var extend = require('util')._extend;
+
+var SAUCELABS_BASE = 'SauceLabs';
+var SAUCELABS_PREFIX = 'SL';
+/*
+module.exports = generateFromRange({
+  // platform
+  'Windows 7': {
+    'chrome': '30..39'
   }
-};
+});
+*/
+function generateFromRange(browserRange){
+
+  return Object
+    .keys(browserRange)
+    .reduce(function (launchers, platformName) {
+      extend(memo,
+        Object
+          .keys(browserRange[platformName])
+          .reduce(function (platformLaunchers, browserName) {
+
+            platformLaunchers[SAUCELABS_PREFIX + '_']
+            return platformLaunchers;
+          }, {})
+      );
+      return memo;
+    }, {});
+/*
+  function processLauncherRange(range){
+    if(typeof range === 'string'){
+      var limits = range.match(/(\d+)..(\d+)/);
+      var min = limits[1], max = limits[2];
+      var delta = max - min;
+      range = Array
+        .call(null, {length : delta})
+        .map(function (v, i){ return min + i; };
+    }
+
+    return range.reduce(function(memo, version){
+      memo.version = version;
+      return memo;
+    }, {
+      // default
+      base: SAUCELABS_BASE
+    });
+  }
+*/
+  function newLauncher(browserName, platformName, version){
+    return {
+      base: SAUCELABS_BASE,
+      browserName: browserName,
+      platform: platformName,
+      version: version
+    };
+  }
+}
+
+function processLauncherRange(range){
+  if(typeof range === 'string'){
+    var limits = range.match(/(\d+)..(\d+)/);
+    var min = +limits[1], max = +limits[2];
+    var delta = max - min;
+    range = Array
+      .apply(null, {length : delta + 1})
+      .map(function (v, i){ return min + i; });
+  }
+
+  return range.map(function(version){
+    return {
+      // default
+      base: SAUCELABS_BASE,
+      version: version
+    };
+  });
+}
+
+function queueLauncherKey(asKey, subprocess){
+ return function processTarget(targets){
+  return Object
+    .keys(targets)
+    .map(function(targetName){
+      return subprocess(targets[targetName])
+        .map(function(launcher){
+          launcher[asKey] = targetName;
+          return launcher;
+        });
+    })
+    .reduce(function(a, b){
+      // flatten
+      return a.concat(b);
+    });
+  };
+}
+
+var processBrowsers = queueLauncherKey('browserName', processLauncherRange);
+var processPlatform = queueLauncherKey('platform', processBrowsers);
+
+// The magic spell !!
+var geSaLaKaCuLa = generateSauceLabsKarmaCustomLaunshers;
+
+function generateSauceLabsKarmaCustomLaunshers(config){
+  return processPlatform(config)
+    .reduce(function(memo, launcher){
+      var launcherName = [
+        SAUCELABS_PREFIX,
+        shorty(launcher.platform),
+        shorty(launcher.browserName),
+        launcher.version
+      ].join('_');
+      memo[launcherName] = launcher;
+      return memo;
+    }, {});
+
+  function shorty(str){
+    return str.indexOf(' ') === -1 ?
+      // capitaliseFirstLetter
+      str.charAt(0).toUpperCase() + str.slice(1):
+      // abbr
+      str.match(/\b(\w)/g).join('').toUpperCase();
+  }
+
+}
+
+// geSaLaKaCuLaaaaaaaa :D
+var foo = geSaLaKaCuLa({
+    'linux': { chrome: '0..1', 'internet explorer': ['9']},
+    'Windows 7': { chrome: '30..39' }
+  });
+
+console.log(foo);
